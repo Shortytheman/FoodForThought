c2ec306cca7e954e4b75fac0665bffad8d8040b5
Hyttel <mikkel.hw@hotmail.com>
2022-02-27 14:03:14 +0100

    Start

--- src/main/java/com/example/foodforthought/controller/Controller.java
+++ src/main/java/com/example/foodforthought/controller/Controller.java
@@ -1,6 +1,21 @@
 package com.example.foodforthought.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Index";
+    }
+    @GetMapping("/Aboutus")
+    public String AboutUs() {
+        return "AboutUs";
+    }
+    @GetMapping("/Beneficials")
+    public String Beneficials() {
+        return "Beneficials";
+    }
 
 
 
