34315e2e44ec6af9c8667e7d3c28026ae4ee80ad
Shortytheman <shor7ty@gmail.com>
2022-02-27 13:48:50 +0100

    Repository

---
+++ src/main/java/com/example/foodforthought/FoodForThoughtApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodforthought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodForThoughtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoodForThoughtApplication.class, args);
+  }
+
+}
